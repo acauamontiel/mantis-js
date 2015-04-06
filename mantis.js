f79fbaf38539d8cae4ecc06c68ec974393d85f69
Acauã Montiel <admin@acauamontiel.com.br>
2015-04-06 00:41:22 -0300

    add Mantis.fn.removeClass

--- mantis.js
+++ mantis.js
@@ -184,6 +184,28 @@
 					this.className += (this.className === '') ? value : ' ' + value;
 				});
 			}
+		},
+		removeClass: function(value) {
+			var v = value.split(' ');
+			if (this[0].classList) {
+				return this.each(function() {
+					var i;
+					for (i = 0; i < v.length; i++) {
+						this.classList.remove(v[i]);
+					}
+				});
+			} else {
+				return this.each(function() {
+					var c = ' ' + this.className.replace(/[\t\r\n]/g, ' ') + ' ',
+						i;
+					for (i = 0; i < v.length; i++) {
+						while (c.indexOf(' ' + v[i] + ' ') >= 0) {
+							c = c.replace(' ' + v[i] + ' ', ' ');
+						}
+					}
+					this.className = c.replace(/^\s+|\s+$/g, '');
+				});
+			}
 		}
 	});
 	return $;
