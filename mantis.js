9d1557db52e0f5ad7eadc7ee3066726fa7c8d988
Acauã Montiel <admin@acauamontiel.com.br>
2015-04-18 07:51:18 -0300

    add Mantis.fn.before

--- mantis.js
+++ mantis.js
@@ -282,6 +282,21 @@
 					}
 				});
 			}
+		},
+		before: function(value) {
+			var length;
+			if (typeof value === 'string') {
+				return this.each(function() {
+					this.insertAdjacentHTML('beforebegin', value);
+				});
+			} else if (value.constructor === Mantis) {
+				length = value.length;
+				return this.each(function() {
+					while (length--) {
+						this.parentNode.insertBefore(value[length], this);
+					}
+				});
+			}
 		}
 	});
 	return $;
--- src/manipulation.js
+++ src/manipulation.js
@@ -48,5 +48,23 @@ Mantis.extend({
 				}
 			});
 		}
+	},
+
+	before: function (value) {
+		var length;
+
+		if (typeof value === 'string') {
+			return this.each(function () {
+				this.insertAdjacentHTML('beforebegin', value);
+			});
+		} else if (value.constructor === Mantis) {
+			length = value.length;
+
+			return this.each(function () {
+				while (length--) {
+					this.parentNode.insertBefore(value[length], this);
+				}
+			});
+		}
 	}
 });
